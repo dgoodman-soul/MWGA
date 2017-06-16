190f3ff00a8164028248e22253f5f0d5608254f2
dgoodman <david.goodman@soul-cycle.com>
2017-06-16 16:44:55 -0400

    Refactos

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 $( document ).ready(function() {
-    clearInterval(mainInterval);
-    doEverything();
+  clearInterval(mainInterval);
+  doEverything();
 });
 
 var getPurpleElements = function() {
-        var divs = $('div[class*=purple]').toArray();
-        var headers = $('header[class*=purple]').toArray();
-        return divs.concat(headers);
-    }
+  var divs = $('div[class*=purple]').toArray();
+  var headers = $('header[class*=purple]').toArray();
+  return divs.concat(headers);
+}
 
-    var setToGreen = function(elements) {
-        for (var i=0; i<elements.length; i++) {
-            element = elements[i];
-            element.style.setProperty('background-color', '#22a230');
-    }
+var setToGreen = function(elements) {
+ for (var i=0; i<elements.length; i++) {
+    element = elements[i];
+    element.style.setProperty('background-color', '#22a230');
+  }
 }
 
 var doEverything = function() {
-    var elements = getPurpleElements();
-    setToGreen(elements);
+  var elements = getPurpleElements();
+  setToGreen(elements);
 }
 
 mainInterval = setInterval(doEverything(), 500);
